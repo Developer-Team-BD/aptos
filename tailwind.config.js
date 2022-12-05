287e49df12ddbbc6851c8f524360f4ca6920871d
antorahmed <antorahmed1616@gmail.com>
2022-12-05 16:00:36 +0500

    Page 4 Initial Release

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        cornerstone: ["Cornerstone", "sans-serif"],
+      },
+      colors:{
+        // Primary Colors
+        lightblack: "#101010",
+        orange: "#FF9703",
+        red: "#EB5651",
+        darkgreen: "#05954CCC",
+        darkred: "#FC5A5AB2"
+      },
+    }
+  },
+  plugins: [],
+}
